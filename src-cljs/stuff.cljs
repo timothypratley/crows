67022d4f8cb4b87198ac6ef3fab73f850c865f12
Timothy Pratley <timothypratley@gmail.com>
2013-12-10 22:00:34 -0800

    added flycontrols

--- src-cljs/stuff.cljs
+++ src-cljs/stuff.cljs
@@ -5,12 +5,16 @@
         width (.-innerWidth js/window)
         height (.-innerHeight js/window)
         camera (js/THREE.PerspectiveCamera. 75 (/ width height) 0.1 1000 )
+        controls (js/THREE.FlyControls. camera)
         renderer (js/THREE.CanvasRenderer.)
         geometry (js/THREE.PlaneGeometry. 8 8 8 8)
         material (js/THREE.MeshPhongMaterial. (clj->js {:color 0x00ff00
                                                         :wireframe true}))
         cube (js/THREE.Mesh. geometry material)
+        clock (js/THREE.Clock.)
         render (fn cb []
+                 (let [delta (.getDelta clock)]
+                   (.update controls delta))
                  (js/requestAnimationFrame cb)
                  (.render renderer scene camera))]
     (dotimes [i (.-length (.-vertices geometry))]
@@ -18,6 +22,12 @@
     (.setSize renderer width height)
     (.appendChild js/document.body (.-domElement renderer) )
     (.add scene cube)
+    (set! (.-domElement controls) renderer)
+    (set! (.-movementSpeed controls) 0.1)
+		(set!	(.-domElement controls) container)
+		(set!	(.-rollSpeed	controls) 0.1)
+		(set!	(.-autoForward	controls) true)
+		(set!	(.-dragToLook	controls) true)
     (set! (.-z (.-position camera))  5)
     (render)))
 
